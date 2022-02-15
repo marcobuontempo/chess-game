b998ca101dc5eb63e84fed55bed6b5d4dad59971
marcobuontempo <marco.buon36@gmail.com>
2022-02-16 00:37:03 +1100

    Setup getters and setters for Chessboard class

--- index.js
+++ index.js
@@ -1,16 +1,76 @@
 class Chessboard {
     constructor(fen) {
         this._fen = fen || "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-        this._fenPosition = this._fen.split(" ")[0],
-        this._fenTurn = this._fen.split(" ")[1],
-        this._fenCastle = this._fen.split(" ")[2],
-        this._fenEnPassant = this._fen.split(" ")[3],
-        this._fenHalfMove = this._fen.split(" ")[4],
-        this._fenFullMove = this._fen.split(" ")[5],
-        this._boardTiles = [],
+        this._turn = "white",
+        this._castleRights = ["K","Q","k","q"],
+        this._enPassantSquare = [null,null],
+        this._halfMoveCount = 0,
+        this._fullMoveCount = 1,
+        this._boardSquares = [],
         this._pieceIcons = { k:"♚", q:"♛", r:"♜", b:"♝", n:"♞", p:"♟", 
-                             K:"♚", Q:"♛", R:"♜", B:"♝", N:"♞", P:"♟" }
+                             K:"♔", Q:"♕", R:"♖", B:"♗", N:"♘", P:"♙" }
     }
 
 
+
+
+    /*
+    =================
+    =====GETTERS=====
+    =================
+    */
+    getFen() {
+        return this._fen;
+    }
+    getTurn() {
+        return this._turn;
+    }
+    getCastleRights() {
+        return this._castleRights;
+    }
+    getEnPassantSquare() {
+        return this._enPassantSquare;
+    }
+    getHalfMoveCount() {
+        return this._halfMoveCount;
+    }
+    getFullMoveCount() {
+        return this._fullMoveCount;
+    }
+    getBoardSquares() {
+        return this._boardSquares;
+    }
+    getPieceIcons() {
+        return this._pieceIcons;
+    }
+
+
+
+
+    /*
+    =================
+    =====SETTERS=====
+    =================
+    */
+    setFen(newFen) {
+        this._fen = newFen;
+    }
+    setTurn(newTurn) {
+        this._turn = newTurn;
+    }
+    setCastleRights(newCastleRights) {
+        this._castleRights = newCastleRights;
+    }
+    setEnPassantSquare(newEnPassantSquare) {
+        this._enPassantSquare = newEnPassantSquare;
+    }
+    setHalfMoveCount(newHalfMoveCount) {
+        this._halfMoveCount = newHalfMoveCount;
+    }
+    setFullMoveCount(newFullMoveCount) {
+        this._fullMoveCount = newFullMoveCount;
+    }
+    setBoardSquares(newBoardSquares) {
+        this._boardSquares = newBoardSquares;
+    }
 }
