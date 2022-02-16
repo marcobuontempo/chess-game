3e164f09d73293bb92a5de43a92f980fbf625351
marcobuontempo <marco.buon36@gmail.com>
2022-02-17 00:24:28 +1100

    Create empty board

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Chessboard {
     constructor(fen) {
-        this._fen = fen || "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        this._fen = fen || "rnbbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
         this._turn = "white",
         this._castleRights = ["K","Q","k","q"],
         this._enPassantSquare = [null,null],
@@ -40,6 +40,9 @@ class Chessboard {
     getBoardSquares() {
         return this._boardSquares;
     }
+    getSquare(boardFile,boardRank) {
+        return this._boardSquares[boardFile-1][boardRank-1];
+    }
     getPieceIcons() {
         return this._pieceIcons;
     }
@@ -73,4 +76,120 @@ class Chessboard {
     setBoardSquares(newBoardSquares) {
         this._boardSquares = newBoardSquares;
     }
+    setSquarePiece(boardFile,boardRank,newPiece) {
+        this._boardSquares[boardFile-1][boardRank-1].hasPiece = newPiece;
+    }
+
+
+
+
+
+    /*
+    ==================
+    ==CONVERT VALUES==
+    ==================
+    */
+   //get piece type from fen key input
+    convertPieceFenKeyToName(fenKey) {
+        switch(fenKey.toLowerCase()) {
+            case "r": return "rook"
+            case "n": return "knight"
+            case "b": return "bishop"
+            case "q": return "queen"
+            case "k": return "king"
+            case "p": return "pawn"
+        }
+    }
+    //get fen key from piece type input
+    convertPieceTypeToFenKey(colour, type) {
+        let fenKey
+        switch(type.toLowerCase()) {
+            case "rook": fenKey = "r"; break
+            case "knight": fenKey = "n"; break
+            case "bishop": fenKey = "b"; break
+            case "queen": fenKey = "q"; break
+            case "king": fenKey = "k"; break
+            case "pawn": fenKey = "p"; break
+        }
+        if(colour=="white") { fenKey = fenKey.toUpperCase() }
+        return fenKey
+    }
+
+
+
+
+    /*
+    =================
+    ==CREATE  BOARD==
+    =================
+    */
+    //generate square coordinate string
+    generateSquareCoordinate(boardFile, boardRank) {
+        let fileLetter;
+        switch(boardFile) {
+            case(1): fileLetter="A"; break
+            case(2): fileLetter="B"; break
+            case(3): fileLetter="C"; break
+            case(4): fileLetter="D"; break
+            case(5): fileLetter="E"; break
+            case(6): fileLetter="F"; break
+            case(7): fileLetter="G"; break
+            case(8): fileLetter="H"; break
+        }
+        return fileLetter+boardRank
+    }
+    //generate square colour
+    generateSquareColour(boardFile,boardRank) {
+        if((boardFile%2==0 && boardRank%2!=0) || (boardFile%2!=0 && boardRank%2==0)) {
+            return "white"
+        } else {
+            return "black"
+        }
+    }
+    //create board square
+    createSquare(squareCoordinate, squareColour, boardFile, boardRank, hasPiece) {
+        return {
+            squareCoordinate,
+            squareColour,
+            boardFile,
+            boardRank,
+            hasPiece
+        }
+    }
+    //create a board piece
+    createPiece(colour,type) {
+        const fenPieceKey = this.convertPieceTypeToFenKey(colour,type)
+        return {
+            fenPieceKey,
+            colour,
+            type,
+            icon: this.getPieceIcons()[fenPieceKey]
+        }
+    }
+    //create empty chessboard
+    createEmptyChessboard() {
+        const boardSquares = [];
+        for(let boardFile=1; boardFile<=8; boardFile++) {
+            const file = [];
+            for(let boardRank=1; boardRank<=8; boardRank++) {
+                const squareCoordinate = this.generateSquareCoordinate(boardFile,boardRank)
+                const squareColour = this.generateSquareColour(boardFile,boardRank)
+                const hasPiece = null
+                const square = this.createSquare(squareCoordinate,squareColour,boardFile,boardRank,hasPiece)
+                file.push(square)
+            }
+            boardSquares.push(file)
+        }
+        this.setBoardSquares(boardSquares)
+    }
+
 }
+
+
+
+
+
+
+
+// ==== T E S T ====
+test = new Chessboard()
